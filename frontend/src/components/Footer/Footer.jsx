bfe60d7c766c3b613f99d598ae982dfc5a108c90
HendyCastro <hendy0702@gmail.com>
2024-06-11 13:58:34 -0400

    configurando Footer

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Footer.css';
+
+
+const Footer = () => {
+
+    return (
+        <Footer>
+            <p> Mi Empresa. Todos los derechos reservados</p>
+        </Footer>
+    );
+}
+
+export default Footer;
