8de5cd3332f89367508a435c337173ab8b097288
HendyCastro <hendy0702@gmail.com>
2024-06-12 23:30:23 -0400

    modificaciones en el navbar, ingreso de img en ini

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,34 +3,51 @@ import { NavLink } from "react-router-dom";
 import { useAuth } from "../../context/Context";
 
 function Navbar() {
+  const { isAuthenticated, logout } = useAuth();
+  const setActiveClass = ({ isActive }) => (isActive ? "active" : "inactive");
 
-    const { isAuthenticated, logout } = useAuth();
-    const setActiveClass = ({ isActive }) => (isActive ? 'active' : 'inactive')
-
-    return (
-        <header>
-            <nav>
-                <div className="buttons">
-                    <NavLink className={`btn ${setActiveClass}`} to="/" >Inicio</NavLink>
-                    <NavLink className={`btn ${setActiveClass}`} to="/vehiculos" >Vehículos</NavLink>
-                    {!isAuthenticated ? (
-                        <>
-                            <NavLink className={`btn ${setActiveClass}`} to="/registro" >Regístrate</NavLink>
-                            <NavLink className={`btn ${setActiveClass}`} to="/login" >Iniciar Sesión</NavLink>
-                        </>
-                    ) : (
-                        <>
-                            <NavLink className={`btn ${setActiveClass}`} to="/mis-publicaciones" >Mis Publicaciones</NavLink>
-                            <NavLink className={`btn ${setActiveClass}`} to="/perfil" >Perfil</NavLink>
-                            <button className="btn" onClick={logout}>Cerrar Sesión</button>
-                        </>
-                    )}
-                    <NavLink className={`btn ${setActiveClass}`} to="/publicar" >Publicar Aviso</NavLink>
-                </div>
-            </nav>
-        </header>
-
-    )
+  return (
+    <header>
+      <nav className="nav" >
+        <div className="buttons">
+          {!isAuthenticated ? (
+            <>
+              <NavLink className={setActiveClass} to="/">
+                <img src="https://via.placeholder.com/300" alt="" />
+              </NavLink>
+              <NavLink active className={setActiveClass} to="/login">
+                Iniciar Sesión
+              </NavLink>
+              <NavLink className={setActiveClass} to="/registro">
+                Registrarse
+              </NavLink>
+              <NavLink className={setActiveClass} to="/vehiculos">
+                Vehiculos
+              </NavLink>
+            </>
+          ) : (
+            <>
+              <NavLink className={setActiveClass} to="/vehiculos">
+                Vehiculos
+              </NavLink>
+              <NavLink className={setActiveClass} to="/publicar">
+                Publicar
+              </NavLink>
+              <NavLink className={setActiveClass} to="/mis-publicaciones">
+                Mis Publicaciones
+              </NavLink>
+              <NavLink className={setActiveClass} to="/perfil">
+                Perfil
+              </NavLink>
+              <NavLink className={setActiveClass} to="/login" onClick={logout}>
+                Cerrar Sesión
+              </NavLink>
+            </>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
---
+++ frontend/src/views/Inicio/Inicio.jsx
@@ -0,0 +1,16 @@
+import './Inicio.css';
+import vehiculo from '../../assets/img/vehiculo1.jpg'
+
+
+
+function Inicio() {
+    return (
+        <>
+            <h1>Este es el inicio</h1>
+            <img src={vehiculo} alt="carro"/>
+        
+        </>
+    )
+}
+
+export default Inicio
