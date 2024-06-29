3023e437cd2e687a05b0ca4fabac8aee8cb0e14b
Jhonny Quevedo <quevedojhonny@gmail.com>
2024-06-29 12:48:02 -0400

    prueba

--- backend/database.js
+++ backend/database.js
@@ -2,11 +2,18 @@ const { Pool } = require("pg")
 require("dotenv").config()
 
 const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+
+module.exports = { pool }
+
+/* const pool = new Pool({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     allowExitOnIdle: true
-})
-
-module.exports = { pool }
+}) */
